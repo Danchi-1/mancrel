31e2d56d9de71f6243ca3bd247c40b8d16de1b4e
Ohachor Daniel <ohachordaniel2021@gmail.com>
2025-12-11 09:12:16 +0100

    fix: Corrected error causing vercel deployment to fail

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,60 @@
-self.__BUILD_MANIFEST={"polyfillFiles":["static/chunks/polyfills.js"],"devFiles":[],"ampDevFiles":[],"lowPriorityFiles":["static/development/_buildManifest.js","static/development/_ssgManifest.js"],"rootMainFiles":[],"pages":{"/_app":[]},"ampFirstPages":[]}
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/": [
+      "static/chunks/4fceb80db6279b76.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/0ed167d55875e6b3.js",
+      "static/chunks/turbopack-5189b81c202364cc.js"
+    ],
+    "/_app": [
+      "static/chunks/9f26e155a05cc475.js",
+      "static/chunks/4fceb80db6279b76.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/555d1b8fefe7f8c2.css",
+      "static/chunks/turbopack-777051f13c82879c.js"
+    ],
+    "/_error": [
+      "static/chunks/b3f6db8249113d61.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/turbopack-56ea29027b81aa2c.js"
+    ],
+    "/dashboard": [
+      "static/chunks/5ed2e05b071c6da8.js",
+      "static/chunks/4fceb80db6279b76.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/turbopack-a77b02d8a8daebc2.js"
+    ],
+    "/signin": [
+      "static/chunks/425d949bddbdd8ad.js",
+      "static/chunks/4fceb80db6279b76.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/turbopack-8f4dd92a7ed1e8a0.js"
+    ],
+    "/signup": [
+      "static/chunks/62480417d131ca01.js",
+      "static/chunks/4fceb80db6279b76.js",
+      "static/chunks/db2bfe74d307fa57.js",
+      "static/chunks/6c74ac7b38aa2ad6.js",
+      "static/chunks/ded1c2990370e649.js",
+      "static/chunks/turbopack-7f3dee10e1f7d9d7.js"
+    ]
+  },
+  "devFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
